39d02f852a47c332032d403de4292791b8955768
Smirnov Peter <sh18rw@yandex.ru>
2024-08-07 12:26:03 +0300

    Initial commit

---
+++ client/src/main/java/client/App.java
@@ -0,0 +1,9 @@
+package client;
+
+import ru.sh18rw.network.TestNetwork;
+
+public class App {
+	public static void main(String[] args) {
+		TestNetwork.testMethod("Hello, client!");
+	}
+}
---
+++ server/src/main/java/server/App.java
@@ -0,0 +1,9 @@
+package server;
+
+import ru.sh18rw.network.TestNetwork;
+
+public class App {
+	public static void main(String[] args) {
+		TestNetwork.testMethod("Hello, server!");
+	}
+}
